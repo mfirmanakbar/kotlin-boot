e183c7c3aa04c9d92d0b61a5e67811cb76b2ee90
mfirmanakbar <em.firman.akbar@gmail.com>
2020-02-02 17:01:08 +0700

    initial commit

---
+++ src/test/kotlin/com/mfirmanakbar/kotlinboot/KotlinBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mfirmanakbar.kotlinboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
